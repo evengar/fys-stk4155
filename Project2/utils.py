f556e5cf8224a7fab34e44b6b13e3a9646e839bd
evengar <evengar@uio.no>
2024-10-11 13:58:03 +0200

    Finished assignment and rendered

--- Project2/utils.py
+++ Project2/utils.py
@@ -5,3 +5,13 @@ def data_function(x):
 
 def poly_index(maxpoly):
     return(sum(np.arange(maxpoly + 1) + 1))
+
+def sub_to_poly(X_train, X_test, maxpoly, remove_intercept = True, interaction = False):
+    if interaction:
+        X_train_npoly = X_train[:,int(remove_intercept):poly_index(maxpoly)]
+        X_test_npoly = X_test[:,int(remove_intercept):poly_index(maxpoly)]
+    else:
+        X_train_npoly = X_train[:,int(remove_intercept):maxpoly+1]
+        X_test_npoly = X_test[:,int(remove_intercept):maxpoly+1]
+
+    return X_train_npoly, X_test_npoly
